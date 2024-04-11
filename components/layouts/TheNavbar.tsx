2587b64b1f30ceab404d0f93051c8ebcec67b2e3
Jesutooni <edeworuyoyou@gmail.com>
2024-04-11 07:59:08 +0100

    navbar ui

--- components/layouts/TheNavbar.tsx
+++ components/layouts/TheNavbar.tsx
@@ -1,5 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+import MobileNav from "./MobileNav";
+
 const TheNavbar = () => {
-  return <div>TheNavbar</div>;
+  return (
+    <nav className="flex justify-between items-center fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          alt="boom logo"
+          width={32}
+          height={32}
+          className="max-sm:size-10 "
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Boom
+        </p>
+      </Link>
+
+      <div className="flex justify-between items-center gap-5">
+        {/* Clerk - User Management */}
+
+        <MobileNav />
+      </div>
+    </nav>
+  );
 };
 
 export default TheNavbar;
